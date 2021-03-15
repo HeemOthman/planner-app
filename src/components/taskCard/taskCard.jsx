595574bff0a3217c165e96b9d26331fb9e4b073b
Heem Othman <heem.othman@gmail.com>
2021-03-14 22:06:57 -0500

    Various Task Card Updates
    -Added ability to change the text in each task.
    
    -Added ability to add tasks to a task card.
    
    -Added ability to remove tasks and added a trash icon to go with it.
    
    -Changed the delete task card icon to match the add task icon.
    
    -Fixed some bugs that came with adding and removing tasks. The code now references the current index of the item versus the item id.

--- src/components/taskCard/taskCard.jsx
+++ src/components/taskCard/taskCard.jsx
@@ -22,21 +22,29 @@ class TaskCard extends Component {
                         onChange={this.handleChange}>
                     </input>
 
-                    <span className="removeCard" onClick={() => this.props.onDelete(this.props.id)}>X</span>
+                    <span className="gg-add-r" onClick={this.handleAddTask}></span>
+                    <span className="gg-close-r" onClick={() => this.props.onDelete(this.props.id)}></span>
                 </div>
                 
                 <div className="body">
                     <ul>
                         {this.state.tasks.map(task =>
-                            <div className="task" key={task.id}>
+                            <div className="taskList" key={task.id}>
                                 <input type="checkbox"
                                        className="checkbox"
                                        id={task.id}
-                                       onChange={() => this.handleCheckboxClick(this.state.tasks[task.id])}
-                                       checked={task.isComplete}></input>
-                                <li className="taskDescription"
-                                    id={task.id}
-                                    style={task.isComplete? {"textDecoration": "line-through"} : {} }>{task.value}</li>
+                                       onChange={() => this.handleCheckboxClick(task)}
+                                       checked={task.isComplete}>
+                                </input>
+                                <li>
+                                    <input className="taskDescription"
+                                           id={task.id}
+                                           style={task.isComplete? {"textDecoration": "line-through"} : {} }
+                                           defaultValue={task.value}
+                                           onChange={(event) => this.handleChangeTask(event, task)}>
+                                    </input>
+                                </li>
+                                <span className="gg-trash" onClick={() => this.handleRemoveTask(task)}></span>
                             </div>
                         )}
                     </ul>
@@ -54,20 +62,55 @@ class TaskCard extends Component {
     }
 
 
-    //HANDLE CLICKING THE CHECKBOX
-    handleCheckboxClick = (task) => {
-        console.log(task.id);
-
+    //HANDLE CHANGING TITLE TEXT
+    handleChangeTask = (event, task) => {
         const updatedTasks = [...this.state.tasks];
-        updatedTasks[task.id] = {...task};
-        updatedTasks[task.id].isComplete = !updatedTasks[task.id].isComplete;
+        let index = updatedTasks.indexOf(task);
+        updatedTasks[index].value = event.target.value;
 
-         this.setState({
+        this.setState({
+            tasks: updatedTasks
+        })
+    }
+
+
+//TODO: If adding, then deleting, then adding tasks back,
+//will get an error about encountering children with the same id.
+//Need to update the id's of each item based on their current index
+
+    //HANDLE ADDING A TASK
+    handleAddTask = () => {
+        this.setState({
+            tasks: [...this.state.tasks, {id: this.state.tasks.length, value: "New Task", isComplete: false}]
+        });
+    }
+
+
+    //HANDLE REMOVING A TASK
+    handleRemoveTask = (task) => {
+        const updatedTasks = [...this.state.tasks];
+        let index = updatedTasks.indexOf(task);
+
+        updatedTasks.splice(index, 1);
+
+        this.setState({
             tasks: updatedTasks
         });
-
-        console.log(updatedTasks[task.id].isComplete);
     }
+
+
+    //HANDLE CLICKING THE CHECKBOX
+    handleCheckboxClick = (task) => {
+        const updatedTasks = [...this.state.tasks];
+        let index = updatedTasks.indexOf(task);
+
+        updatedTasks[index].isComplete = !updatedTasks[index].isComplete;
+
+        this.setState({
+            tasks: updatedTasks
+        });
+    }
+
 }
  
 export default TaskCard;
